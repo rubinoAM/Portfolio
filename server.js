27a91898968818db95946b3a1743acafc93768fd
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-07 09:45:31 -0500

    Responsive and general styling tweaks

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+app.set('views','views');
+app.set('view engine','ejs');
+
+app.get('/',(req,res,next)=>{
+    res.render('index',{});
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(4444);
-console.log("LIstening")
+console.log("Listening");
